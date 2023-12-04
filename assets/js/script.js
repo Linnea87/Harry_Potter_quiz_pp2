788e282814ccaeb6a42c02e471ac8f5ae8fbb5a1
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-04 16:37:50 +0000

    set up basic structure to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+function startGame() {
+
+}
+
+function updateScore() {
+
+}
+
+function renderScore() {
+
+}
+
+function setNextQuestion(){
+
+}
+
+function showQuestion(question) {
+
+}
+
+function resetState() {
+
+}
+
+function selectAnswer(e) {
+
+}
+
+function settingStatus(element, correct) {
+
+}
+
+function decrementScore() {
+
+}
+
+function clearUp(element) {
+
+}
+
+function displayEndScore() {
+    
+}
