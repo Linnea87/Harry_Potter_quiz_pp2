72b1673873ff091a0efe3dfd6370fec5544a7139
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-05 23:15:09 +0000

    Set up structure and add content to questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,5 +1,190 @@
-// questions used for the quiz 
+// Questions used for the quiz 
 
 const questions = [
-    
-]
+ 
+    {
+        question: "Which of these is NOT one of the unforgivable curses?",
+        answers:[
+            {text:"Curse of the Cruciatus", correct: false},
+            {text:"Imperius' Curse", correct: false},
+            {text:"sectumsempra", correct: true},
+            {text:"Avada Kedavra", correct: false},
+
+        ]
+    }, 
+    {
+        question: "How does Harry manage to breathe underwater during the second task of the Triwizard Tournament?",
+        answers:[
+            {text:"He turns into a shark", correct: false},
+            {text:"He kisses a mermaid", correct: false},
+            {text:"He eats gillyweed", correct: true},
+            {text:"He performs a bubble-head charm", correct: false},
+
+        ]
+    }, 
+    {
+        question: "Who guards the entrance to the Gryffindor common room?",
+        answers:[
+            {text:"The Gray Lady", correct: false},
+            {text:"The fat brother", correct: false},
+            {text:"The Bloody Baron", correct: false},
+            {text:"The fat lady", correct: true},
+
+        ]
+    },
+    {
+        question: "Who is NOT a member of the Order of the Phoenix?",
+        answers:[
+            {text:"Cornelius Fudge", correct: true},
+            {text:"Mad-eye Moody", correct: false},
+            {text:"Professor Snape", correct: false},
+            {text:"Remus Lupin", correct: false},
+
+        ]
+    },
+    {
+        question: "What does the spell Obliviate do?",
+        answers:[
+            {text: "Destroy objects", correct: false},
+            {text: "Sending someone to the Netherworld", correct: false},
+            {text: "Removes parts of someone's memory", correct: true },
+            {text: "Make objects invisible", correct: false},
+        ]   
+    },
+    {
+        question: "The three types of balls used in Quidditch are Bludgers, Snitches and...?",
+        answers:[
+            {text: "Quaffles", correct: true},
+            {text: "Wiffles", correct: false},
+            {text: "Bocce", correct: false},
+            {text: "Foozles", correct: false},
+        ]   
+    }, 
+    {
+        question: "Who has stolen Harry's letter from Ron and Hermione at the beginning of Harry Potter and the Chamber of Secrets?",
+        answers:[
+            {text: "Dumbledor", correct: false},
+            {text: "Draco Malfoy", correct: false},
+            {text: "Dobby", correct: true},
+            {text: "The Dursleys", correct: false},
+        ]
+    },
+    {
+        question: "How many Weasley siblings are there?",
+        answers:[
+            {text:"5", correct: false},
+            {text:"7", correct: true},
+            {text:"10", correct: false},
+            {text:"3", correct: false},
+        ]
+    },
+    {
+        question: "From which King's Cross platform does the Hogwarts Express leave?",
+        answers:[
+            {text:"A quarter past eight", correct: false},
+            {text:"A quarter past nine", correct: true},
+            {text:"Five and a half", correct: false},
+            {text:"A quarter past eleven", correct: false},
+        ]
+    },
+    {
+        question: "What model is the first broom Harry ever gets?",
+        answers:[
+            {text:"Cleansweep One", correct: false},
+            {text:"Nimbus 2000", correct: true},
+            {text:"Hoover", correct: false},
+            {text:"firebolt", correct: false},
+        ]
+    },
+    {
+        question: "Which professor teaches flying lessons?",
+        answers:[
+            {text:"Professor Grubbly-Plank", correct: false},
+            {text:"Sybill Trelawney", correct: false},
+            {text:"Charity Burbage", correct: false},
+            {text:"Mrs. Hooch", correct: true},
+        ]
+    },
+    {
+        question: "What are the names of Draco Malfoy's two cronies?",
+        answers:[
+            {text:"Huggs and Pucey", correct: false},
+            {text:"Flint and Boyle", correct: false},
+            {text:"Crabbe and Goyle", correct: true},
+            {text:"Pike and Zabini", correct: false},
+        ]
+    }, 
+    {
+        question: "Which Patronus belongs to Luna Lovegood?",
+        answers:[
+            {text:"Hind", correct: false},
+            {text:"Rabbit", correct: true},
+            {text:"Dog", correct: false},
+            {text:"Horse", correct: false},
+        ]
+    }, 
+    {
+        question: "What spell did Harry use to kill Lord Voldemort?",
+        answers:[
+            {text:"Expelliarmus", correct: false},
+            {text:"Expecto Patronum", correct: false},
+            {text:"Avada Kedavra", correct: true},
+            {text:"Accio", correct: false},
+        ]
+    },
+    {
+        question:"What element is associated with Hufflepuff?",
+        answers:[
+            {text:"Nature", correct: false},
+            {text:"Earth", correct: true},
+            {text:"Air", correct: false},
+            {text:"Water", correct: false},
+        ]
+    },
+    {
+        question:"Which house values courage, bravery and chivalry?",
+        answers:[
+            {text:"Ravenclaw", correct: false},
+            {text:"Slytherin", correct: false},
+            {text:"Gryffindor", correct: true},
+            {text:"Hufflepuff", correct: false},
+        ]
+    },
+    {
+        question:"Which of these is a type of Love Potion?",
+        answers:[
+            {text:"Felixfelicis", correct: false},
+            {text:"Polyjuice poition", correct: false},
+            {text:"Amortentia", correct: true},
+            {text:"Veritaserum", correct: false},
+        ]
+    },
+    {
+        question:"Which of these are not one of Hagrid's many pets?",
+        answers:[
+            {text:"Fluffy", correct: false},
+            {text:"Grawp", correct: true},
+            {text:"Aragog", correct: false},
+            {text:"Norberta", correct: false},
+        ]
+    }, 
+    {
+        question:"Which class did Severus Snape always want to teach?",
+        answers:[
+            {text:"Defence against the Dark Art", correct: true},
+            {text:"Potions", correct: false},
+            {text:"Charms", correct: false},
+            {text:"Transfiguration", correct: false},
+        ]
+    },
+    {
+        question:"Which Hogwarts house did Moaning Myrtle belong in?",
+        answers:[
+            {text:"Gryffindor", correct: false},
+            {text:"Slytherin", correct: false},
+            {text:"Ravenclaw", correct: true},
+            {text:"Hufflepuff", correct: false},
+        ]
+    },   
+
+];
