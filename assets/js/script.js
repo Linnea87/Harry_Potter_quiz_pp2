4cb1604c775a294399ada429802b74690132bd5d
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-06 10:33:46 +0000

    Add more functions to gamestart section, add new variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 //code being used from other creators have been outlined next to the function
-const startButton = document.getElementById('start-button');
 
+const startButton = document.getElementById('start-button');
+const questionContainer = document.getElementById('question-container');
+
+/* Eventlistener that makes the different buttons visible */
 startButton.addEventListener('click', startGame);
 
 function startGame(){
     console.log('started');
+    startButton.classList.add('hide');
+    questionContainer.classList.remove('hide');
+    setNextQuestion()
 }
 
 function setNextQuestion(){
@@ -14,3 +20,4 @@ function setNextQuestion(){
 function selectAnswer() {
 
 }
+
