444f1f457c36f3bd4344bbd94d1137304ce46a7a
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-06 19:57:08 +0000

    Add new functions and variables in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,36 @@
 
 // Variables 
 const startButton = document.getElementById('start-button');
+const nextbutton = document.getElementById('next-button');
 const questionContainer = document.getElementById('question-container');
 const theQuestions = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
-
+const rulesButton = document.getElementById('game-rules-button')
+const aboutGame = document.getElementById('about-game')
 let shuffledQuestions;
+let currentQuestionIndex;
 
 // Eventlistener that makes the different buttons visible */
 startButton.addEventListener('click', startGame);
-answerButtons.addEventListener('click', selectAnswer);
+button.addEventListener('click', selectAnswer);
 
 function startGame(){
-    console.log('started');
     // Hide buttons that are not suposed to show 
     startButton.classList.add('hide');
+    rulesButton.classList.add('hide');
+    aboutGame.classList.add('hide');
+
      // Shuffle the questions and initialize variables
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
     questionContainer.classList.remove('hide');
     setNextQuestion()
+
 }
 
 //Next question. Code from Web Dev Simplified's Javascript tutorial and modified
 function setNextQuestion(){
+    resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
@@ -41,7 +48,15 @@ function showQuestion(question) {
         answerButtons.appendChild(button);
     });
    
-}   
+}  
+
+function resetState() {
+    nextbutton.classList.add('hide');
+    while (answerButtons.firstChildChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
 function selectAnswer() {
 
 }
