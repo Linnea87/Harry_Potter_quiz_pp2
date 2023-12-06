32928d6d6a1662f57be536d526fb8072ad837b3e
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-06 09:43:02 +0000

    Set up new clean basic structure to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 //code being used from other creators have been outlined next to the function
 
+function startGame() {
+
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer() {
+    
+}
