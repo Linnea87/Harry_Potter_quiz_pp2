dd990c5926177cf9a2c51a5bed10960c87398514
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-06 11:43:42 +0000

    Add functions to questions section, and  new variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,45 @@
 //code being used from other creators have been outlined next to the function
 
+// Variables 
 const startButton = document.getElementById('start-button');
 const questionContainer = document.getElementById('question-container');
+const theQuestions = document.getElementById('question');
+const answerButtons = document.getElementById('answer-buttons');
 
-/* Eventlistener that makes the different buttons visible */
+let shuffledQuestions;
+
+// Eventlistener that makes the different buttons visible */
 startButton.addEventListener('click', startGame);
+answerButtons.addEventListener('click', selectAnswer);
 
 function startGame(){
     console.log('started');
+    // Hide buttons that are not suposed to show 
     startButton.classList.add('hide');
+     // Shuffle the questions and initialize variables
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
     questionContainer.classList.remove('hide');
     setNextQuestion()
 }
 
 function setNextQuestion(){
-
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
-
+// Load the questions and answer options into the buttons on the page. Code used from WebDev Simplified's Javascript tutorial and modified
+function showQuestion(question) {
+    theQuestions.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('button');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        answerButtons.appendChild(button);
+    });
+   
+}   
 function selectAnswer() {
 
 }
