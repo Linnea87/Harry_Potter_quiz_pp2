50d6a2d88496690013026898572540df534d74fc
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-07 22:28:54 +0000

    Add score function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,19 @@
 
 // Variables 
 const startButton = document.getElementById('start-button');
-const nextbutton = document.getElementById('next-button');
+const rulesButton = document.getElementById('game-rules-button');
+const answerButtons = document.getElementById('answer-buttons');
+const resultsContainer = document.getElementById('results-container');
 const questionContainer = document.getElementById('question-container');
 const theQuestions = document.getElementById('question');
-const answerButtons = document.getElementById('answer-buttons');
-const rulesButton = document.getElementById('game-rules-button');
 const aboutGame = document.getElementById('about-game');
+const nextbutton = document.getElementById('next-button');
+const score = document.getElementById('score');
+
 
 let shuffledQuestions;
 let currentQuestionIndex = 0;
+let currentScore = 0;
 
 // Eventlistener that makes the different buttons visible */
 startButton.addEventListener('click', startGame);
@@ -25,11 +29,28 @@ function startGame(){
     // Shuffle the questions and initialize variables
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
+    //Clear the current score
+    currentScore = 0;
+    renderScore(); // Update the score 
+    score.classList.remove('hide')
     questionContainer.classList.remove('hide');
     setNextQuestion();
+    answerButtons.classList.remove('hide');
+    resultsContainer.classList.remove('hide');
 
 }
 
+//update the player's score
+
+function updateScore() {
+    currentScore++ //increment the score
+    renderScore();
+}
+
+function renderScore(){
+    score.innerText = currentScore
+
+}
 //Next question. Code from Web Dev Simplified's Javascript tutorial and modified
 function setNextQuestion(){
     resetState();
