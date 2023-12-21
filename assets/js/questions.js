9c261f6084ce87c799e81adaf0b2398191713aee
Linnea Ternevik <linnea.ternevik@gmail.com>
2023-12-21 23:20:38 +0000

    update the styling

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,7 +3,7 @@
 const questions = [
  
     {
-        question: "Which of these is NOT one of the unforgivable curses ?",
+        question: "Which of these is NOT one of the unforgivable curses?",
         answers:[
             {text:"Curse of the Cruciatus", correct: false},
             {text:"Imperius' Curse", correct: false},
@@ -15,7 +15,7 @@ const questions = [
         imageAlt:"Image of when  mad eye moody teaches about the unforgivable curses ",
     }, 
     {
-        question: "How does Harry manage to breathe underwater ?",
+        question: "How does Harry manage to breathe underwater?",
         answers:[
             {text:"He turns into a shark", correct: false},
             {text:"He kisses a mermaid", correct: false},
@@ -27,7 +27,7 @@ const questions = [
         imageAlt:"Image of the second task of the Triwizard Tournament",
     }, 
     {
-        question: "Who guards the Gryffindor common room ?",
+        question: "Who guards the Gryffindor common room?",
         answers:[
             {text:"The Gray Lady", correct: false},
             {text:"The fat brother", correct: false},
@@ -39,7 +39,7 @@ const questions = [
         imageAlt:"Image of a wall with some of the hogwarts paintings  ",
     },
     {
-        question: "Who is NOT a member of the Order of the Phoenix ?",
+        question: "Who is NOT a member of the Order of the Phoenix?",
         answers:[
             {text:"Cornelius Fudge", correct: true},
             {text:"Mad-eye Moody", correct: false},
@@ -51,7 +51,7 @@ const questions = [
         imageAlt:"Image of some of the Order of the Phoenix members",
     },
     {
-        question: "What does the spell Obliviate do ?",
+        question: "What does the spell Obliviate do?",
         answers:[
             {text: "Destroy objects", correct: false},
             {text: "Sending someone to the Netherworld", correct: false},
@@ -73,7 +73,7 @@ const questions = [
         imageAlt:"Image of the different Quidditch balls ",   
     }, 
     {
-        question: "How stop harry's letter from Ron and Hermione ?",
+        question: "How stop harry's letter from Ron and Hermione?",
         answers:[
             {text: "Dumbledor", correct: false},
             {text: "Draco Malfoy", correct: false},
@@ -84,7 +84,7 @@ const questions = [
         imageAlt:"Image of the wall where you can read that the Chamber of Secrets has opend",
     },
     {
-        question: "How many Weasley siblings are there ?",
+        question: "How many Weasley siblings are there?",
         answers:[
             {text:"5", correct: false},
             {text:"7", correct: true},
@@ -95,7 +95,7 @@ const questions = [
         imageAlt:"Image of some of the Weasley siblings",
     },
     {
-        question: "From which platform does the Hogwarts Express leave ?",
+        question: "From which platform does the Hogwarts Express leave?",
         answers:[
             {text:"Platform 8 3/4", correct: false},
             {text:"Platform 9 3/4", correct: true},
@@ -106,7 +106,7 @@ const questions = [
         imageAlt:"Image of the Hogwarts Express",
     },
     {
-        question: "What model is the first broom Harry ever gets ?",
+        question: "What model is the first broom Harry ever gets?",
         answers:[
             {text:"Cleansweep One", correct: false},
             {text:"Nimbus 2000", correct: true},
@@ -118,7 +118,7 @@ const questions = [
         
     },
     {
-        question: "Which professor teaches flying lessons ?",
+        question: "Which professor teaches flying lessons?",
         answers:[
             {text:"Professor Grubbly-Plank", correct: false},
             {text:"Sybill Trelawney", correct: false},
@@ -129,7 +129,7 @@ const questions = [
         imageAlt:"Image of flying lessons",
     },
     {
-        question: "What are the names of Draco Malfoy's two cronies ?",
+        question: "What are the names of Draco Malfoy's two cronies?",
         answers:[
             {text:"Huggs and Pucey", correct: false},
             {text:"Flint and Boyle", correct: false},
@@ -140,7 +140,7 @@ const questions = [
         imageAlt:"Image of Draco Malfoy, Crabbe and Goyle",
     }, 
     {
-        question: "Which Patronus belongs to Luna Lovegood ?",
+        question: "Which Patronus belongs to Luna Lovegood?",
         answers:[
             {text:"Hind", correct: false},
             {text:"Rabbit", correct: true},
@@ -151,7 +151,7 @@ const questions = [
         imageAlt:"Image of Luna Lovegood",
     }, 
     {
-        question: "What spell did Harry use to kill Lord Voldemort ?",
+        question: "What spell did Harry use to kill Lord Voldemort?",
         answers:[
             {text:"Expelliarmus", correct: true},
             {text:"Expecto Patronum", correct: false},
@@ -162,7 +162,7 @@ const questions = [
         imageAlt:"Imgae of Harry Potter and Lord Voldermort",
     },
     {
-        question:"What element is associated with Hufflepuff ?",
+        question:"What element is associated with Hufflepuff?",
         answers:[
             {text:"Nature", correct: false},
             {text:"Earth", correct: true},
@@ -173,7 +173,7 @@ const questions = [
         imageAlt:"Image of Hufflepuff emblem",
     },
     {
-        question:"Which house values courage, bravery and chivalry ?",
+        question:"Which house values courage, bravery and chivalry?",
         answers:[
             {text:"Ravenclaw", correct: false},
             {text:"Slytherin", correct: false},
@@ -184,7 +184,7 @@ const questions = [
         imageAlt:"Image of the four houses emblems",
     },
     {
-        question:"Which of these is a type of Love Potion ?",
+        question:"Which of these is a type of Love Potion?",
         answers:[
             {text:"Felixfelicis", correct: false},
             {text:"Polyjuice poition", correct: false},
@@ -195,7 +195,7 @@ const questions = [
         imageAlt:"Image of a potion lessons",
     },
     {
-        question:"Which of these are not one of Hagrid's many pets ?",
+        question:"Which of these are not one of Hagrid's many pets?",
         answers:[
             {text:"Fluffy", correct: false},
             {text:"Grawp", correct: true},
@@ -206,7 +206,7 @@ const questions = [
         imageAlt:"Image of Hagrid and Fang",
     }, 
     {
-        question:"Which class did Severus Snape always want to teach ?",
+        question:"Which class did Severus Snape always want to teach?",
         answers:[
             {text:"Defence against the Dark Art", correct: true},
             {text:"Potions", correct: false},
@@ -217,7 +217,7 @@ const questions = [
         imageAlt:"Image of Severus Snape",
     },
     {
-        question:"What Hogwarts house was Moaning Myrtle in ?",
+        question:"What Hogwarts house was Moaning Myrtle in?",
         answers:[
             {text:"Gryffindor", correct: false},
             {text:"Slytherin", correct: false},
